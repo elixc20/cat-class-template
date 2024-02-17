bbadbe7c3417816120fdf907ccca7c9e9b055b5d
elixc20 <elicombs2020@gmail.com>
2024-02-16 19:00:19 -0600

    Update cat.py

--- cat.py
+++ cat.py
@@ -1,7 +1,7 @@
 #Revmoe pass and complete the cat class
 class Cat():
     def __init__(self):
-        self.name = "unknown"
+        self.name = "Unknown"
         self.age = 0
 
     def speak(self):
